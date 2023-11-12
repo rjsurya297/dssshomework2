c7228760c092e8cac686f178bb3b2852b8a0dd9a
rjsurya297 <150440461+rjsurya297@users.noreply.github.com>
2023-11-12 10:15:20 +0100

    Update code_cleanup.py

--- code_cleanup.py
+++ code_cleanup.py
@@ -1,1 +1,66 @@
+import random
+
+def generate_random_integer(min_value, max_value):
+    """
+    Generate a random integer between min_value and max_value (inclusive).
+    """
+    return random.randint(min_value, max_value)
+
+def generate_random_operator():
+    """
+    Generate a random arithmetic operator from the list: ['+', '-', '*'].
+    """
+    return random.choice(['+', '-', '*'])
+
+def evaluate_expression(num1, num2, operator):
+    """
+    Evaluate the expression based on the given numbers and operator.
+    Returns a tuple containing the expression string and the correct answer.
+    """
+    expression = f"{num1} {operator} {num2}"
+
+    if operator == '+':
+        answer = num1 + num2
+    elif operator == '-':
+        answer = num1 - num2
+    else:
+        answer = num1 * num2
+
+    return expression, answer
+
+def math_quiz():
+    """
+    Conduct a math quiz game, where the user needs to answer random arithmetic questions.
+    """
+    score = 0
+    total_questions = 3
+
+    print("Welcome to the Math Quiz Game!")
+    print("You will be presented with math problems, and you need to provide the correct answers.")
+
+    for _ in range(total_questions):
+        num1 = generate_random_integer(1, 10)
+        num2 = generate_random_integer(1, 5)
+        operator = generate_random_operator()
+
+        problem, correct_answer = evaluate_expression(num1, num2, operator)
+
+        print(f"\nQuestion: {problem}")
+        
+        try:
+            user_answer = int(input("Your answer: "))
+        except ValueError:
+            print("Invalid input! Please enter a valid integer.")
+            user_answer = 0  # Assigning a default value for an incorrect input
+
+        if user_answer == correct_answer:
+            print("Correct! You earned a point.")
+            score += 1
+        else:
+            print(f"Wrong answer. The correct answer is {correct_answer}.")
+
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
+
+if __name__ == "__main__":
+    math_quiz()
 
